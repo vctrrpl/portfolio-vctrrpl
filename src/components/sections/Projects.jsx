146bf1f9619820504a827cf34a13482fe6a3c5e6
Victor Ivo Paul <victorivo.paul@gmail.com>
2025-02-19 23:22:38 +0800

    Updated projects styling

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,3 +1,5 @@
+import { MdKeyboardDoubleArrowRight } from 'react-icons/md';
+
 export const Projects = () => {
   return (
     <section
@@ -5,7 +7,7 @@ export const Projects = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <div className="max-w-5xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-orange-500 to-amber-500 bg-clip-text text-transparent text-left">
+        <h2 className="text-3xl font-bold mb-6 bg-gradient-to-r from-orange-500 to-amber-500 bg-clip-text text-transparent text-left">
           My Projects
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -18,24 +20,29 @@ export const Projects = () => {
               Cupiditate, cum.
             </p>
             <div className="flex flex-wrap gap-2 mb-4">
-              {['React', 'Node.js', 'ExpressJS', 'PostgreSQL'].map(
-                (tech, index) => (
-                  <span
-                    key={index}
-                    className="bg-zinc-800/60 text-gray-400 py-1 px-3 rounded-full text-sm hover:bg-zinc-800/20 hover:shadow-gray-300 transition"
-                  >
-                    {tech}
-                  </span>
-                )
-              )}
+              {[
+                'React',
+                'Tailwind CSS',
+                'Node.js',
+                'ExpressJS',
+                'PostgreSQL',
+              ].map((tech, index) => (
+                <span
+                  key={index}
+                  className="bg-zinc-800/60 text-gray-400 py-1 px-3 rounded-full text-sm hover:bg-zinc-800/20 hover:shadow-gray-300 transition"
+                >
+                  {tech}
+                </span>
+              ))}
             </div>
 
             <div className="flex justify-between items-center">
               <a
                 href="#"
-                className="text-gray-400 hover:text-orange-500 transition-colors my-4"
+                className="text-gray-400 hover:text-orange-500 transition-colors my-4 flex items-center"
               >
                 View Project
+                <MdKeyboardDoubleArrowRight className="ml-0.5 text-lg" />
               </a>
             </div>
           </div>
@@ -48,24 +55,29 @@ export const Projects = () => {
               Cupiditate, cum.
             </p>
             <div className="flex flex-wrap gap-2 mb-4">
-              {['React', 'Node.js', 'ExpressJS', 'PostgreSQL'].map(
-                (tech, index) => (
-                  <span
-                    key={index}
-                    className="bg-zinc-800/60 text-gray-400 py-1 px-3 rounded-full text-sm hover:bg-zinc-800/20 hover:shadow-gray-300 transition"
-                  >
-                    {tech}
-                  </span>
-                )
-              )}
+              {[
+                'React',
+                'Tailwind CSS',
+                'Node.js',
+                'ExpressJS',
+                'PostgreSQL',
+              ].map((tech, index) => (
+                <span
+                  key={index}
+                  className="bg-zinc-800/60 text-gray-400 py-1 px-3 rounded-full text-sm hover:bg-zinc-800/20 hover:shadow-gray-300 transition"
+                >
+                  {tech}
+                </span>
+              ))}
             </div>
 
             <div className="flex justify-between items-center">
               <a
                 href="#"
-                className="text-gray-400 hover:text-orange-500 transition-colors my-4"
+                className="text-gray-400 hover:text-orange-500 transition-colors my-4 flex items-center"
               >
                 View Project
+                <MdKeyboardDoubleArrowRight className="ml-0.5 text-lg" />
               </a>
             </div>
           </div>
@@ -78,24 +90,29 @@ export const Projects = () => {
               Cupiditate, cum.
             </p>
             <div className="flex flex-wrap gap-2 mb-4">
-              {['React', 'Node.js', 'ExpressJS', 'PostgreSQL'].map(
-                (tech, index) => (
-                  <span
-                    key={index}
-                    className="bg-zinc-800/60 text-gray-400 py-1 px-3 rounded-full text-sm hover:bg-zinc-800/20 hover:shadow-gray-300 transition"
-                  >
-                    {tech}
-                  </span>
-                )
-              )}
+              {[
+                'React',
+                'Tailwind CSS',
+                'Node.js',
+                'ExpressJS',
+                'PostgreSQL',
+              ].map((tech, index) => (
+                <span
+                  key={index}
+                  className="bg-zinc-800/60 text-gray-400 py-1 px-3 rounded-full text-sm hover:bg-zinc-800/20 hover:shadow-gray-300 transition"
+                >
+                  {tech}
+                </span>
+              ))}
             </div>
 
             <div className="flex justify-between items-center">
               <a
                 href="#"
-                className="text-gray-400 hover:text-orange-500 transition-colors my-4"
+                className="text-gray-400 hover:text-orange-500 transition-colors my-4 flex items-center"
               >
                 View Project
+                <MdKeyboardDoubleArrowRight className="ml-0.5 text-lg" />
               </a>
             </div>
           </div>
@@ -108,24 +125,29 @@ export const Projects = () => {
               Cupiditate, cum.
             </p>
             <div className="flex flex-wrap gap-2 mb-4">
-              {['React', 'Node.js', 'ExpressJS', 'PostgreSQL'].map(
-                (tech, index) => (
-                  <span
-                    key={index}
-                    className="bg-zinc-800/60 text-gray-400 py-1 px-3 rounded-full text-sm hover:bg-zinc-800/20 hover:shadow-gray-300 transition"
-                  >
-                    {tech}
-                  </span>
-                )
-              )}
+              {[
+                'React',
+                'Tailwind CSS',
+                'Node.js',
+                'ExpressJS',
+                'PostgreSQL',
+              ].map((tech, index) => (
+                <span
+                  key={index}
+                  className="bg-zinc-800/60 text-gray-400 py-1 px-3 rounded-full text-sm hover:bg-zinc-800/20 hover:shadow-gray-300 transition"
+                >
+                  {tech}
+                </span>
+              ))}
             </div>
 
             <div className="flex justify-between items-center">
               <a
                 href="#"
-                className="text-gray-400 hover:text-orange-500 transition-colors my-4"
+                className="text-gray-400 hover:text-orange-500 transition-colors my-4 flex items-center"
               >
                 View Project
+                <MdKeyboardDoubleArrowRight className="ml-0.5 text-lg" />
               </a>
             </div>
           </div>
