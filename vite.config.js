eb4c3a4ff6d298da7543ffbc68c66709cee1e37d
Victor Ivo Paul <victorivo.paul@gmail.com>
2025-02-20 20:10:12 +0800

    Updated favicon

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
