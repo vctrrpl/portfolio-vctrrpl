fb10f98919f9dafb3873cd0a54d72abfada18bdb
Victor Ivo Paul <victorivo.paul@gmail.com>
2025-02-20 17:41:02 +0800

    Removed reveal on scroll

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -8,7 +8,6 @@ export const About = () => {
       id="about"
       className="min-h-screen flex items-center justify-center py-20"
     >
-      {' '}
       <div className="max-w-5xl mx-auto px-4 flex items-center flex-col w-full sm:w-full md:w-full">
         <div className="rounded-xl p-8 border-white/20 border hover:-translate-y-1 transition-all w-full">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-orange-500 to-amber-500 bg-clip-text text-transparent">
