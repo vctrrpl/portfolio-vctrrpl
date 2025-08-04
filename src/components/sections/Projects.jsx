0c3f9505dc768ea38ea92d03cdfef7a5a8ff8ee6
Victor Ivo Paul <victorivo.paul@gmail.com>
2025-08-04 13:52:50 +0800

    uploaded project 3

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -105,6 +105,43 @@ export const Projects = () => {
               </div>
             </div>
           </div>
+
+          {/* Project 3 */}
+          <div className="rounded-xl border border-white/20 hover:-translate-y-1 transition">
+            <img
+              src="/project-3-img.jpg"
+              alt="Lee Soon Seng Plastic Industries Sdn. Bhd."
+              className="w-full h-auto object-cover rounded-t-xl"
+            />
+            <div className="p-6">
+              <h3 className="text-xl text-gray-400 font-bold mb-2">
+                LUFX AI Trainer
+              </h3>
+              <p className="text-gray-400 mb-4">
+                45-page WordPress website for a leading plastic manufacturer
+                using Elementor Pro's advanced page builder capabilities.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {['WordPress', 'Elementor'].map((tech, index) => (
+                  <span
+                    key={index}
+                    className="bg-zinc-800/60 text-gray-400 py-1 px-3 rounded-full text-sm hover:bg-white/20 hover:text-white/70 hover:shadow-white transition"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://benxonpack.com/"
+                  className="text-gray-400 hover:text-orange-500 transition-colors my-4 flex items-center"
+                >
+                  View Project
+                  <MdKeyboardDoubleArrowRight className="ml-0.5 text-lg" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
